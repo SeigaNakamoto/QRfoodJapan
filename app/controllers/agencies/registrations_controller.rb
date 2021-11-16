6cf301687966b40ff97b1e9fa0989d2a6b953a8a
SEIGA NAKAMOTO <s.nakamoto.bit@gmail.com>
2021-11-17 03:36:27 +0900

    parent agency : 000 and 001

--- app/controllers/agencies/registrations_controller.rb
+++ app/controllers/agencies/registrations_controller.rb
@@ -13,20 +13,27 @@ class Agencies::RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
     if resource.parent_agency_id.eql?("parent")
-        parent_no = sprintf("%02d", Agency.where(parent_agency_id: "parent").count + 1)
-        resource.agency_id = "Q#{parent_no}-000"
-        # 代理店テーブルの親代理店の数をカウント。2桁の数値に変換し、代理店IDを作成
-      else
-        parent_no = resource.parent_agency_id[1, 2]
-        agency_no = sprintf("%03d", Agency.where('agency_id like ?',"Q#{parent_no}%").count)
-        resource.agency_id = "Q#{parent_no}-#{agency_no}"
-        # 親代理店IDの2桁の数値を格納（例：「Q03-000」の場合、『03』）
-        # 今回が何店舗目の傘下代理店登録か、Agencyテーブルにある代理店IDをカウントし、代理店IDを作成
-      end  
+      # 代理店テーブルの親代理店の数をカウント。2桁の数値に変換し、代理店IDを作成
+      @parent_no = sprintf("%02d", Agency.where(parent_agency_id: "parent").count + 1)
+      resource.agency_id = "Q#{@parent_no}-000"
+    else
+      parent_no = resource.parent_agency_id[1, 2]
+      agency_no = sprintf("%03d", Agency.where('agency_id like ?',"Q#{parent_no}%").count)
+      resource.agency_id = "Q#{parent_no}-#{agency_no}"
+      # 親代理店IDの2桁の数値を格納（例：「Q03-000」の場合、『03』）
+      # 今回が何店舗目の傘下代理店登録か、Agencyテーブルにある代理店IDをカウントし、代理店IDを作成
+    end  
     resource.save
     yield resource if block_given?
     if resource.persisted?
       if resource.active_for_authentication?
+        # 案件獲得用親代理店アカウント作成
+        if resource.parent_agency_id.eql?("parent")
+          agency = Agency.new(sign_up_params)
+          agency.agency_id = "Q#{@parent_no}-001"
+          agency.parent_agency_id = "Q#{@parent_no}-000"
+          agency.save
+        end
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
