074f55a1c4f235acf7e2d555849ed96f1ef2f874
SEIGA NAKAMOTO <s.nakamoto.bit@gmail.com>
2021-11-19 13:54:07 +0900

    change: varidation parent_agnecy_id

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -72,7 +72,7 @@ class Agency < ApplicationRecord
   validates :bank_account_holder_kana, presence: true
 
   def parent_agency_id_check
-    unless Agency.where(agency_id: parent_agency_id).exists?
+    unless parent_agency_id.blank? || Agency.where(agency_id: parent_agency_id).exists?
       errors.add(:parent_agency_id, 'この代理店IDは登録されていません')
     end
   end
