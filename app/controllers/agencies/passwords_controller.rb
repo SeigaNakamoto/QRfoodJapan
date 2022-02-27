dd9890e1064df988168ffd391b155bd8f8708dca
宮北　力也 <miyakita@anconsulting.jp>
2022-02-28 03:02:29 +0900

    no message

--- app/controllers/agencies/passwords_controller.rb
+++ app/controllers/agencies/passwords_controller.rb
@@ -23,9 +23,9 @@ class Agencies::PasswordsController < Devise::PasswordsController
 
   # protected
 
-  # def after_resetting_password_path_for(resource)
-  #   super(resource)
-  # end
+  def after_resetting_password_path_for(resource)
+    new_agency_session_path
+  end
 
   # The path used after sending reset password instructions
   # def after_sending_reset_password_instructions_path_for(resource_name)
