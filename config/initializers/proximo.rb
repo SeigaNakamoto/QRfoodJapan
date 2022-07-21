b8fd4326cf98e126c0a08de8eb5f0028c81d7a61
ricky <hurley_sukisugiru@yahoo.co.jp>
2022-07-21 10:34:41 +0900

    一旦削除

--- config/initializers/proximo.rb
+++ config/initializers/proximo.rb
@@ -2,11 +2,11 @@ require "rest-client"
 
 # RestClient.proxy = ENV["PROXIMO_URL"] if ENV["PROXIMO_URL"]
 
-RestClient::Request.execute(
-  method: :get,
-  url: 'https://stg-qr-food-japan-management.herokuapp.com/users/entry_payment?agency_id=Q01-004',
-  proxy: ENV["PROXIMO_URL"]
-)
+# RestClient::Request.execute(
+#   method: :get,
+#   url: 'https://stg-qr-food-japan-management.herokuapp.com/users',
+#   proxy: ENV["PROXIMO_URL"]
+# )
 
   # puts "status code", res.code
 # puts "headers", res.headers
