fd6eed27fc38eeb8177a204942113bb03128f137
ricky <hurley_sukisugiru@yahoo.co.jp>
2022-07-21 17:39:54 +0900

    no message

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,7 +1,7 @@
 class IndexController < ApplicationController
   def letsencrypt
     if params[:id] == ENV["LETSENCRYPT_REQUEST"]
-    render text: ENV["LETSENCRYPT_RESPONSE"]
+      render plain: ENV["LETSENCRYPT_RESPONSE"]
     end
   end
 end
